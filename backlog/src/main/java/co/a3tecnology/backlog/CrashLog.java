ab9cfff016f6be754d4386002e783069748857d6
antoniodahora-dev <antonio_dahora@hotmail.com>
2023-06-15 15:32:35 -0300

    biblioteca

---
+++ backlog/src/main/java/co/a3tecnology/backlog/CrashLog.java
@@ -0,0 +1,10 @@
+package co.a3tecnology.backlog;
+
+import android.util.Log;
+
+public final class CrashLog {
+
+    public static void print(String message) {
+        Log.d("CrashLog", message);
+    }
+}
---
+++ crashlog/src/main/java/co/a3tecnology/crashlog/DebugLog.java
@@ -0,0 +1,10 @@
+package co.a3tecnology.crashlog;
+
+import android.util.Log;
+
+public final class DebugLog {
+
+    public static void print(String message) {
+        Log.d("Debugloh", message);
+    }
+}
